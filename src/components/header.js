7b049a9f345975e12f47bb56304f374fbfe2f341
Khaled Garbaya <khaledgarbaya@gmail.com>
2019-07-24 23:20:37 +0200

    Wrap logo in a link

--- src/components/header.js
+++ src/components/header.js
@@ -19,12 +19,14 @@ const Header = ({ siteTitle }) => (
       </Link>
     </nav>
     <div className="container lg:max-w-screen-lg mx-auto px-6 py-8 sm:py-16 md:py-24 text-white">
-      <img
-        src="/logo.svg"
-        className="mb-12 block"
-        width="200"
-        alt="A JAM sandiwch wearing glasses"
-      />
+      <Link to="/">
+        <img
+          src="/logo.svg"
+          className="mb-12 block"
+          width="200"
+          alt="A JAM sandiwch wearing glasses"
+        />
+      </Link>
       <div className="mb-12">
         <h1 className="font-display font-semibold text-white text-4xl sm:text-5xl md:text-6xl mb-4 leading-none">
           Static sites can do so much more!
