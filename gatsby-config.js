671fa1b2b70476fc81a0b4b2df77c5409f069248
Khaled Garbaya <khaledgarbaya@gmail.com>
2019-08-25 18:57:11 +0200

    Add convertkit and rssfeed

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,13 @@
+const {
+  documentToPlainTextString
+} = require('@contentful/rich-text-plain-text-renderer')
+
 module.exports = {
   siteMetadata: {
     title: `Learn JAMstack`,
     description: `Learnjamstack | Learn how build apps that combine the performance and workflow benefits of static sites with the flexibility and power of server-driven sites.`,
     author: `@khaled_garbaya`,
-    siteUrl: `https://learnjamstack.com`,
+    siteUrl: `https://learnjamstack.com`
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -12,14 +16,14 @@ module.exports = {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `images`,
-        path: `${__dirname}/src/images`,
-      },
+        path: `${__dirname}/src/images`
+      }
     },
     {
       resolve: `gatsby-source-contentful`,
       options: {
         spaceId: process.env.LEARN_JAMSTACK_SPACE_ID,
-        accessToken: process.env.LEARN_JAMSTACK_CDA_KEY,
+        accessToken: process.env.LEARN_JAMSTACK_CDA_KEY
       }
     },
     `gatsby-transformer-sharp`,
@@ -33,25 +37,86 @@ module.exports = {
         background_color: `#ffffff`,
         theme_color: `#4dc0b5`,
         display: `minimal-ui`,
-        icon: `src/images/learnjamstack-icon.png`, // This path is relative to the root of the site.
-      },
+        icon: `src/images/learnjamstack-icon.png` // This path is relative to the root of the site.
+      }
     },
     `gatsby-plugin-postcss`,
     {
       resolve: 'gatsby-plugin-purgecss',
       options: {
         tailwind: true,
-        purgeOnly: ['src/css/style.css'], // Purge only tailwind
-      },
+        purgeOnly: ['src/css/style.css'] // Purge only tailwind
+      }
+    },
+    {
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        query: `
+          {
+            site {
+              siteMetadata {
+                title
+                description
+                siteUrl
+              }
+            }
+            
+          }
+        `,
+        feeds: [
+          {
+            query: `
+            {
+              allContentfulJamStackEntry {
+                edges {
+                  node {
+                    category
+                    createdAt
+                    externalLink
+                    id
+                    title
+                    updatedAt
+                    childContentfulJamStackEntryTextRichTextNode {
+                      json
+                    }
+                  }
+                }
+              }
+            }
+            `,
+            serialize: ({ query: { site, allContentfulJamStackEntry } }) => {
+              return allContentfulJamStackEntry.edges.map(edge => {
+                return Object.assign(
+                  {},
+                  {
+                    description: !edge.node
+                      .childContentfulJamStackEntryTextRichTextNode
+                      ? ''
+                      : documentToPlainTextString(
+                          edge.node.childContentfulJamStackEntryTextRichTextNode
+                            .json
+                        ),
+                    date: edge.node.createdAt,
+                    url: edge.node.externalLink,
+                    guid: edge.node.id
+                  }
+                )
+              })
+            },
+            output: 'feed.xml',
+            title: 'Learn JAMstack RSS Feed'
+          }
+        ]
+      }
     },
     {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
-        trackingId: "UA-145154358-1",
+        trackingId: 'UA-145154358-1',
         head: false,
         anonymize: true,
-        siteSpeedSampleRate: 10,
-      },
-    },
-  ],
+        siteSpeedSampleRate: 10
+      }
+    }
+  ]
 }
--- src/components/subscribe-form.js
+++ src/components/subscribe-form.js
@@ -1,41 +1,42 @@
-import React from 'react';
+import React from 'react'
 
 function SubscribeForm() {
   return (
-      <form 
-        action="https://tinyletter.com/kgarbaya" 
-        method="post"
-        target="popupwindow"
-        onSubmit={() => {
-          window.open(
-            'https://tinyletter.com/kgarbaya',
-            'popupwindow',
-            'scrollbars=yes,width=800,height=600',
-          )
-          return true
-        }}
-      >
-        <p className="text-lg sm:text-xl text-white font-semibold mb-4">
-          <span className="hidden sm:inline">
-            Get a weekly Digest in your inbox{" "}
-          </span>{" "}
-          <span className="text-blue-light ml-1">→</span>
-        </p>
-        <div className="max-w-sm sm:flex">
-          <input type="hidden" value="1" name="embed" />
-          <input
-            type="email"
-            className="focus:outline-none block text-lg sm:text-xl w-full bg-white rounded sm:rounded-r-none px-6 py-3 sm:py-4 mb-2 sm:mb-0"
-            name="email"
-            placeholder="Enter your email"
-            required=""
-          />
-          <button className="focus:outline-none focus:bg-blue sm:text-md w-full sm:w-auto bg-blue-dark hover:bg-blue rounded sm:rounded-l-none uppercase text-white font-bold tracking-wide py-3 px-6 sm:py-4">
-            Begin learning
-          </button>
-        </div>
-      </form>
-  );
+    <form
+      action="https://app.convertkit.com/forms/1036024/subscriptions"
+      method="post"
+      target="popupwindow"
+      onSubmit={e => {
+        e.preventDefault()
+        window.open(
+          'https://pages.convertkit.com/55c3df4a07/8e001d610f',
+          'popupwindow',
+          'scrollbars=yes,width=800,height=600'
+        )
+        return true
+      }}
+    >
+      <p className="text-lg sm:text-xl text-white font-semibold mb-4">
+        <span className="hidden sm:inline">
+          Get a weekly Digest in your inbox{' '}
+        </span>{' '}
+        <span className="text-blue-light ml-1">→</span>
+      </p>
+      <div className="max-w-sm sm:flex">
+        <input type="hidden" value="1" name="embed" />
+        <input
+          type="email"
+          className="focus:outline-none block text-lg sm:text-xl w-full bg-white rounded sm:rounded-r-none px-6 py-3 sm:py-4 mb-2 sm:mb-0"
+          name="email_address"
+          placeholder="Enter your email"
+          required=""
+        />
+        <button className="focus:outline-none focus:bg-blue sm:text-md w-full sm:w-auto bg-blue-dark hover:bg-blue rounded sm:rounded-l-none uppercase text-white font-bold tracking-wide py-3 px-6 sm:py-4">
+          Begin learning
+        </button>
+      </div>
+    </form>
+  )
 }
 
-export default SubscribeForm;
+export default SubscribeForm
